fc5e3a47668223e03211a6461dd7dd723f45928f
prranavv <pranavvkumar03@gmail.com>
2025-02-06 18:00:29 +0530

    Fixed Total staked button

--- Frontend/FrontEnd_Staking/src/Balance.jsx
+++ Frontend/FrontEnd_Staking/src/Balance.jsx
@@ -4,38 +4,20 @@ import { useState } from "react";
 
 export function TotalBalance(){
     const [enabled,setEnabled] = useState(false)
-    const {data,isLoading,error}=useReadContract({
+    const {data,isLoading}=useReadContract({
         abi:abi,
         address:"0x610178dA211FEF7D417bC0e6FeD39F05609AD788",
         functionName:'total',
-        
     })
-    console.log(data,error)
+
+    const handleclick=()=>{
+        setEnabled(!enabled)
+    }
 
     return (
         <>
-            {/* <button onClick={()=>{setEnabled(!enabled)}} disabled={isLoading}>
-                Total Staked
-            </button>
-            {} */}
-            {data?.toString()}
+            <button onClick={handleclick} disabled={isLoading}>Total Staked in Contract</button>
+            {enabled && <div>{data?.toString()}</div>}
         </>
     )
-    if(isLoading){
-        return <div>Loading...</div>
-      }
-
-      if (error)
-        return (
-          <div>
-            Error: {(error).shortMessage || error.message}
-          </div>
-        )
-      return <div>
-        Total Supply is {data?.toString()}
-      </div>
-    // console.log(data)
-    // return <div>
-    //     You have staked {(data?.toString())}  ETH
-    // </div>
 }
